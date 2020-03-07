3c5513334039dc6e4102053989db6f91f01ad7bf
TarunKhatana <tarunkhatana1987@gmail.com>
2020-03-07 23:44:46 +0530

    Initial code setup of exchange api service

---
+++ AspNetCore2.1/ExchangeApi/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExchangeApiService.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        [HttpGet("{email}")]
+        public ActionResult<string> Get(string email)
+        {
+            return email;
+        }
+
+    }
+}
